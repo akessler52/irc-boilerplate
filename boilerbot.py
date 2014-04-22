31c60937d53c05817257f1a9ba14f63a94c0901d
Danny Dyla <dyladan@gmail.com>
2014-04-22 01:00:05 -0400

    Made standards compliant and Python 3 compatible
    python 3 style print statements
    
    now fully complies with python 3 PEP8 according to pylint
    
    broken down into functions for easier readability

--- boilerbot.py
+++ boilerbot.py
@@ -1,48 +1,82 @@
 #!/usr/bin/env python
+"""BOILERBOT.py
+
+This code is intended to be a simple boilerplate
+for building a simple python bot.
+To better understand whats goin on,
+I recommend going through and commenting
+every single line with its purpose in the bot.
+
+https://docs.python.org/2/
+https://tools.ietf.org/html/rfc2812i
+"""
+
+import socket
+
+HOST = "localhost" #yakko.cs.wmich.edu
+PORT = 6667
+NICK = "boilerbot"
+IDENT = 'boilerbot'
+REALNAME = 'boilerbot'
+CHANNEL = "#asdf"
+
+SOCK = socket.socket()
+print("Connecting to server: " + HOST)
+SOCK.connect((HOST, PORT))
 
 
-#BOILERBOT.py                         
+def sendraw(string):
+    """encode and send to server without processing"""
+    print('>', string.strip())
+    SOCK.send(string.encode())
 
-# This code is intended to be a simple boilerplate for building a simple python bot.
-# To better understand whats goin on, I recommend going through and commenting 
-# every single line with its purpose in the bot.
-# https://docs.python.org/2/
-# https://tools.ietf.org/html/rfc2812i
+def privmsg(message):
+    """Send a message to CHANNEL"""
+    msg = "PRIVMSG %s :%s\r\n" % (CHANNEL, message)
+    sendraw(msg)
 
-import socket              
-import string
-import this
-                    
-HOST ="dot" #yakko.cs.wmich.edu  
-PORT = 6667                
-NICK ="boilerbot"                                                                
-IDENT = 'boilerbot'                                                                
-REALNAME = 'boilerbot'                                                            
-readbuffer = ""                                                                  
-channel = "#asdf"                                                                
-readbuffer = ''                                                                  
-                                                                                 
-s = socket.socket( )                                                             
-print "Connecting to server: "+HOST                                              
-                                                                                 
-s.connect((HOST, PORT))                                                          
-s.send("NICK %s\r\n" % NICK)                                                     
-s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))                       
-s.send("JOIN #asdf\r\n")                                                         
-s.send("PRIVMSG %s :CAWWW, CAWW\r\n" % channel)                
-                                                                                 
+def nick(nickname):
+    """Set IRC nick"""
+    sendraw("NICK %s\r\n" % nickname)
 
-print NICK,"Running"                                                           
-                                                                                 
-                                                                                 
-def sendmessage(message):                                                        
-        s.send('PRIVMSG ' +channel+' :'+message+'\r\n')                          
-                                                                                  
-while 1:                                                                         
-        data = s.recv(2048)                                                      
-        print data                                                                           
-        if data.find('PING') != -1:                                              
-                s.send('PONG' + data.split()[1]+'\r\n')                          
-        if data.find("test") != -1:                                             
-                sendmessage("BOILER BOT ALIVE")      
-             
+def user(ident, name):
+    """Set IRC user"""
+    sendraw("USER %s 0 * :%s\r\n" % (ident, name))
+
+def join(chan):
+    """Join IRC channel"""
+    sendraw("JOIN %s\r\n" % chan)
+
+def pong(response):
+    """Send PONG response to server PING"""
+    sendraw("PONG %s\r\n" % response)
+
+def listen():
+    """Listen forever on socket"""
+    while 1:
+        data = SOCK.recv(2048).decode()
+        for line in data.splitlines():
+            print(line)
+            if 'PING' == line.split()[0]:
+                pong(line.split()[1])
+            if 'PRIVMSG' in line:
+                if '!test' in line:
+                    privmsg("BOILER BOT ALIVE")
+
+                if 'roll call' in line:
+                    privmsg("CAWW, CAWW")
+
+                if ':hi boilerbot' in line:
+                    sender = line.split("!")[0][1:]
+                    privmsg("%s: hi" % sender)
+
+
+if __name__ == "__main__":
+    nick(NICK)
+    user(IDENT, REALNAME)
+    join(CHANNEL)
+
+    privmsg("Hello, I am %s" % NICK)
+    print(NICK, "Running")
+
+    listen()
